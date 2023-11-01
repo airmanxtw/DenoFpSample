c188973e001ee06583755f11741910f9505e8acf
airmanxtw <airmanx@stust.edu.tw>
2023-11-01 23:49:44 +0800

    init

--- composables/useAdd1.ts
+++ composables/useAdd1.ts
@@ -1,10 +1,10 @@
 export const useAdd1 = () => {
-    const add1 = (a: number, b: number) => {
-        if(a>0 && b>0) {
-            return a + b
-        } else {
-            throw new Error('Both numbers must be positive');
-        }
+  const add1 = (a: number, b: number) => {
+    if (a > 0 && b > 0) {
+      return a + b;
+    } else {
+      throw new Error("Both numbers must be positive");
     }
-    return { add1 }
-}
+  };
+  return { add1 };
+};
