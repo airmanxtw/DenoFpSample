83d7c630425a285452f789efca04998f2e70688f
airmanxtw <airmanx@stust.edu.tw>
2023-11-02 00:03:19 +0800

    init

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,7 @@
-//import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
-//import { add } from "./main.ts";
+import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
+import { useAddMonads } from "./composables/useAddMonads.ts";
+const { add } = useAddMonads();
 
 Deno.test(function addTest() {
-  //assertEquals(add(2, 3), 5);
+  assertEquals(add(2, 3).unwrap(), 5);
 });
