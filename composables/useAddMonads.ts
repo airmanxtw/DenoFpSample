22b721a4568a3ea283fecb371779147bdfa95f76
airmanxtw <airmanx@stust.edu.tw>
2023-11-03 00:34:26 +0800

    add example

--- composables/useAddMonads.ts
+++ composables/useAddMonads.ts
@@ -1,6 +1,8 @@
-import { Ok, Err } from "monads";
+import { Ok, Err, Result } from "monads";
 export const useAddMonads = () => {
-  const add = (a: number, b: number) =>
+  const add = (a: number, b: number): Result<number, string> =>
     a > 0 && b > 0 ? Ok(a + b) : Err("Both numbers must be positive");
-  return { add };
+  const square = (a: number) => a * a;
+  const half = (a: number) => a / 2;
+  return { add, square, half };
 };
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+import { useAddMonads } from "./composables/useAddMonads.ts";
+const { add, square, half } = useAddMonads();
 
-
-
+add(2, 3)
+  .map(square)
+  .map(half)
+  .match({
+    ok: (n) => console.log(`Result: ${n}`),
+    err: (s) => console.log(`Error: ${s}`),
+  });
